e274644fe6acd5c523419f792af3916a55e951ed
Rocio <r.venegasrojas.06@gmail.com>
2025-08-15 21:04:22 -0400

    first comment

---
+++ bases/app.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let msg = 'Hola Mundo!!!!';
+let hero = {
+    name: 'Ironman',
+    age: 45
+};
+console.log({ hero });
---
+++ bases/app.ts
@@ -0,0 +1,9 @@
+
+let msg: string = 'Hola Mundo!!!!';
+
+let hero = {
+    name: 'Ironman',
+    age: 45
+}
+
+console.log({hero});
