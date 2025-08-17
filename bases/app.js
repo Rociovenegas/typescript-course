c05f05ddeb26905502d986dd82a5de48b4b0edcf
Rocio <r.venegasrojas.06@gmail.com>
2025-08-17 00:53:20 -0400

    chore(homework): add practice exercise 2

--- bases/app.js
+++ bases/app.js
@@ -1,38 +1,27 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-(() => {
-    // Tipos
-    const batman = 'Bruce';
-    const superman = 'Clark';
-    const existe = false;
-    // Tuplas
-    const parejaHeroes = [batman, superman];
-    const villano = ['Lex Lutor', 5, true];
-    // Arreglos
-    const aliados = ['Mujer Maravilla', 'Acuaman', 'San', 'Flash'];
-    //Enumeraciones
-    let fuerzaSuperHeroes;
-    (function (fuerzaSuperHeroes) {
-        fuerzaSuperHeroes[fuerzaSuperHeroes["fuerzaAcuaman"] = 0] = "fuerzaAcuaman";
-        fuerzaSuperHeroes[fuerzaSuperHeroes["fuerzaBatman"] = 1] = "fuerzaBatman";
-        fuerzaSuperHeroes[fuerzaSuperHeroes["fuerzaFlash"] = 5] = "fuerzaFlash";
-        fuerzaSuperHeroes[fuerzaSuperHeroes["fuerzaSuperman"] = 100] = "fuerzaSuperman";
-    })(fuerzaSuperHeroes || (fuerzaSuperHeroes = {}));
-    const fuerzaAcuaman = fuerzaSuperHeroes.fuerzaAcuaman;
-    const fuerzaBatman = fuerzaSuperHeroes.fuerzaBatman;
-    const fuerzaFlash = fuerzaSuperHeroes.fuerzaFlash;
-    const fuerzaSuperman = fuerzaSuperHeroes.fuerzaSuperman;
-    //console.log({ fuerzaSuperHeroes });
-    // Retorno de funciones
-    function activar_batiseñal() {
-        return 'activada';
+// Funciones Básicas
+function sumar(a, b) {
+    return a + b;
+}
+const contar = (heroes) => {
+    return heroes.length;
+};
+const superHeroes = ["Flash", "Arrow", "Superman", "Linterna Verde"];
+contar(superHeroes);
+//Parametros por defecto
+const llamarBatman = (llamar) => {
+    if (llamar) {
+        console.log("Batiseñal activada");
     }
-    function pedir_ayuda() {
-        console.log('Auxilio!!!');
-    }
-    // Aserciones de Tipo
-    const poder = '100';
-    //const largoDelPoder:number = (poder.length);
-    const largoDelPoder = poder.length;
-    console.log(largoDelPoder);
-})();
+};
+llamarBatman();
+// Rest?
+const unirheroes = (personas) => {
+    return personas.join(", ");
+};
+// Tipo funcion
+const noHaceNada = (numero, texto, booleano, arreglo) => { };
+// Crear el tipo de funcion que acepte la funcion "noHaceNada"
+let noHaceNadaTampoco;
+noHaceNadaTampoco = noHaceNada;
