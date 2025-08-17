68c8be57c9774ff260ed3df3aa01152b3130c133
Rocio <r.venegasrojas.06@gmail.com>
2025-08-17 01:24:36 -0400

    feat(homework): exercise 2 completed

--- bases/app.js
+++ bases/app.js
@@ -1,27 +1,55 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 // Funciones Básicas
-function sumar(a, b) {
-    return a + b;
-}
-const contar = (heroes) => {
-    return heroes.length;
-};
+// function sumar( a, b ){
+//   return a + b;
+// }
+Object.defineProperty(exports, "__esModule", { value: true });
+const sumar = (a, b) => a + b;
+const sumarTest = sumar(5, 6);
+console.log(sumarTest);
+// const contar = ( heroes ) => {
+//   return heroes.length;
+// }
+const contar = (heroes) => heroes.length;
 const superHeroes = ["Flash", "Arrow", "Superman", "Linterna Verde"];
-contar(superHeroes);
+const contarTest = contar(superHeroes);
+console.log(contarTest);
 //Parametros por defecto
-const llamarBatman = (llamar) => {
-    if (llamar) {
-        console.log("Batiseñal activada");
-    }
-};
+// const llamarBatman = ( llamar ) => {
+//   if( llamar ){
+//     console.log("Batiseñal activada");
+//   }
+// }
+const llamarBatman = (llamar = false) => llamar ? console.log("Batiseñal!") : console.log("No Batiseñal :c");
 llamarBatman();
+llamarBatman(true);
 // Rest?
-const unirheroes = (personas) => {
-    return personas.join(", ");
+// const unirheroes = ( personas ) => {
+//   return personas.join(", ");
+// }
+const unirheroes = (...heroes) => {
+    return heroes.join(", ");
 };
+const unirTest = unirheroes("Flash", "Arrow", "Superman", "Linterna Verde");
+console.log(`unirTest: ${unirTest}`);
 // Tipo funcion
-const noHaceNada = (numero, texto, booleano, arreglo) => { };
-// Crear el tipo de funcion que acepte la funcion "noHaceNada"
-let noHaceNadaTampoco;
-noHaceNadaTampoco = noHaceNada;
+// const noHaceNada = ( numero, texto, booleano, arreglo )=> {}
+const printNumber = (a) => console.log(`printNumber: ${a}`);
+const printString = (text) => console.log(`printText: ${text}`);
+const printBoolean = (someBoolean) => console.log(`printboolean: ${someBoolean}`);
+const printArray = (someArray) => console.log(`printArray: ${someArray}`);
+let myFunctionNumber;
+let myFunctionString;
+let myFunctionBoolean;
+let myFunctionArray;
+myFunctionNumber = printNumber;
+myFunctionString = printString;
+myFunctionBoolean = printBoolean;
+myFunctionArray = printArray;
+myFunctionNumber(5);
+myFunctionString('Hello');
+myFunctionBoolean(true);
+myFunctionArray(['Hello', 'Kittens']);
+// // Crear el tipo de funcion que acepte la funcion "noHaceNada"
+// let noHaceNadaTampoco;
+// noHaceNadaTampoco = noHaceNada
--- bases/app.ts
+++ bases/app.ts
@@ -1,32 +1,69 @@
 // Funciones Básicas
-function sumar( a, b ){
-  return a + b;
-}
+// function sumar( a, b ){
+//   return a + b;
+// }
 
-const contar = ( heroes ) => {
-  return heroes.length;
-}
+const sumar = (a: number, b: number ): number =>  a + b;
+const sumarTest = sumar( 5 ,6 );
+console.log(sumarTest);
+// const contar = ( heroes ) => {
+//   return heroes.length;
+// }
+
+const contar = ( heroes: string[] ): number => heroes.length;
 const superHeroes = ["Flash", "Arrow", "Superman", "Linterna Verde"];
-contar(superHeroes);
+const contarTest = contar(superHeroes);
+console.log(contarTest);
+
 
 //Parametros por defecto
-const llamarBatman = ( llamar ) => {
-  if( llamar ){
-    console.log("Batiseñal activada");
-  }
-}
+// const llamarBatman = ( llamar ) => {
+//   if( llamar ){
+//     console.log("Batiseñal activada");
+//   }
+// }
+
+const llamarBatman = ( llamar:boolean = false ): void =>  llamar? console.log("Batiseñal!"): console.log("No Batiseñal :c");
 
 llamarBatman();
+llamarBatman(true);
 
 // Rest?
-const unirheroes = ( personas ) => {
-  return personas.join(", ");
+// const unirheroes = ( personas ) => {
+//   return personas.join(", ");
+// }
+
+const unirheroes = ( ...heroes: string[] ): string => {
+    return heroes.join(", ");
 }
 
+const unirTest = unirheroes("Flash", "Arrow", "Superman", "Linterna Verde");
+console.log(`unirTest: ${unirTest}`);
 
 // Tipo funcion
-const noHaceNada = ( numero, texto, booleano, arreglo )=> {}
+// const noHaceNada = ( numero, texto, booleano, arreglo )=> {}
 
-// Crear el tipo de funcion que acepte la funcion "noHaceNada"
-let noHaceNadaTampoco;
-noHaceNadaTampoco = noHaceNada
+const printNumber = ( a: number ):void => console.log(`printNumber: ${a}`);
+const printString = ( text: string ):void => console.log(`printText: ${text}`); 
+const printBoolean = ( someBoolean:boolean ):void => console.log(`printboolean: ${someBoolean}`);
+const printArray = ( someArray: string[] ):void => console.log(`printArray: ${someArray}`);
+
+let myFunctionNumber: ( x:number ) => void;
+let myFunctionString: ( x:string ) => void;
+let myFunctionBoolean: ( x:boolean ) => void;
+let myFunctionArray: ( x:string[] ) => void;
+
+myFunctionNumber = printNumber;
+myFunctionString = printString;
+myFunctionBoolean = printBoolean; 
+myFunctionArray = printArray;
+
+myFunctionNumber(5);
+myFunctionString('Hello');
+myFunctionBoolean(true);
+myFunctionArray(['Hello', 'Kittens']);
+
+
+// // Crear el tipo de funcion que acepte la funcion "noHaceNada"
+// let noHaceNadaTampoco;
+// noHaceNadaTampoco = noHaceNada
