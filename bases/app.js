bd08ebb640bad13b7d6d7168923d277c3535944f
Rocio <r.venegasrojas.06@gmail.com>
2025-08-16 22:13:01 -0400

    chore(homework): add practice exercise 1

--- bases/app.js
+++ bases/app.js
@@ -1,10 +1,29 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 (() => {
-    const a = 10;
-    let b = 'Cats';
-    function sayHello(msg) {
-        console.log(`Hello! ${msg}`);
+    // Tipos
+    const batman = 'Bruce';
+    const superman = 'Clark';
+    const existe = false;
+    // Tuplas
+    const parejaHeroes = [batman, superman];
+    const villano = ['Lex Lutor', 5, true];
+    // Arreglos
+    const aliados = ['Mujer Maravilla', 'Acuaman', 'San', 'Flash'];
+    //Enumeraciones
+    const fuerzaFlash = 5;
+    const fuerzaSuperman = 100;
+    const fuerzaBatman = 1;
+    const fuerzaAcuaman = 0;
+    // Retorno de funciones
+    function activar_batiseñal() {
+        return 'activada';
     }
-    sayHello(b);
+    function pedir_ayuda() {
+        console.log('Auxilio!!!');
+    }
+    // Aserciones de Tipo
+    const poder = '100';
+    const largoDelPoder = poder.length;
+    console.log(largoDelPoder);
 })();
--- bases/app.ts
+++ bases/app.ts
@@ -1,15 +1,39 @@
 (() => {
-    const a:number = 10;
-    let b: string = 'Cats'
 
-    function sayHello( msg:string ){
-        console.log(`Hello! ${msg}`);
+    // Tipos
+    const batman  = 'Bruce';
+    const superman = 'Clark';
+  
+    const existe = false;
+  
+    // Tuplas
+    const parejaHeroes = [batman,superman];
+    const villano = ['Lex Lutor',5,true];
+  
+    // Arreglos
+    const aliados = ['Mujer Maravilla','Acuaman','San', 'Flash'];
+  
+    //Enumeraciones
+    const fuerzaFlash = 5;
+    const fuerzaSuperman = 100;
+    const fuerzaBatman = 1;
+    const fuerzaAcuaman = 0;
+  
+    // Retorno de funciones
+    function activar_batiseñal(){
+      return 'activada';
     }
-
-    sayHello(b);
-
-
-})(); 
-
-
-
+  
+    function pedir_ayuda(){
+      console.log('Auxilio!!!');
+    }
+  
+    // Aserciones de Tipo
+    const poder: any = '100';
+    const largoDelPoder:number = poder.length;
+    console.log( largoDelPoder );
+  
+  
+  })()
+  
+  
